5693afa4a2f0efb10cdcd91957040adb58fbaf9f
Macarthur Inbody <133794m3r@gmail.com>
2019-04-22 13:11:29 -0400

    removed console.log

--- question_encoder.js
+++ question_encoder.js
@@ -4,8 +4,6 @@ function encode_question(input_data){
     data_array['correct_answer']=input_data.correct_choice.value;
     data_array['answers']=[input_data.a_0.value,input_data.a_1.value,input_data.a_2.value,input_data.a_3.value];
     var encoded_data=msgpack.encode(data_array);
-    console.log(input_data.correct_choice.value);
-    console.log(encoded_data);
-    console.log(JSON.stringify(data_array));
+
     return 0;
 }
