57ba8595b3b7373df6a0e3c75a8973fa8e6c4a78
Macarthur Inbody <133794m3r@gmail.com>
2019-05-18 00:58:39 -0400

    Updated some files and reorged scripts to their own folder.
    Updated data and also moved files around to finally be able to get it looking
    and being organized like a real site. Plus I've added a TODO file.

--- admin/unitcat_dev.js
+++ admin/unitcat_dev.js
@@ -1,1 +1,18 @@
+"use strict";
+function data_to_modify() {
+    var val=document.getElementById('data_type_to_modify').value;
+    switch(val){
+        case 'M':
+            break;
+        case 0:
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+    }
+    return 0;
+}
 
