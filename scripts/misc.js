6c95af8c8b5d12b74b36355a6b21a22e746446e3
Macarthur Inbody <133794m3r@gmail.com>
2019-05-30 16:53:27 -0400

    readded some code I had to change back.

--- scripts/misc.js
+++ scripts/misc.js
@@ -5,6 +5,7 @@
 * AGPLv3
 */
 "use strict"
+
 function permute(arr) {
   var N = arr.length;
   var directions = [
@@ -71,7 +72,13 @@ function permute(arr) {
     return make_result();
 
 }
-
+/**
+ * This function will select the data by it's id.
+ *
+ * @param   {string} function it's the element id to get
+ *
+ * @returns {DOMObject} The DOMNode representing the element requested.
+ */
 function select_by_id(element_id){
 var element=document.getElementById(element_id);
 return element;
@@ -79,15 +86,51 @@ return element;
 function select_all(identifier){
     return document.querySelectorAll(identifier);
 }
+/**
+ * Ajax wrapper function.
+ *
+ * @param   {string}    location        the location url to send the request to.
+ * @param   {string}    method_type     the method type. E.g GET, POST, PUT.
+ * @param   {string}    data            the data to be sent. It is sent as a string JSON/Msgpack/b64 encoded.
+ *
+ * @return  {string}    response        the data returned is a string representation of the data structures.
+ */
+
 function ajax(location,method_type,data){
     var ajax=new XMLHttpRequest();
-    ajax.open('GET','/dev_broker?crsf=new');
+    var crsf_token='';
+    ajax.open('GET','/dev_broker?crsf=new',false);
     ajax.setRequestHeader('X-Requested-With : XMLHttpRequest');
     ajax.send();
+    crsf_token=ajax.response;
+    if(method_type ===('GET' || 'PUT' || 'POST')){
+        return 1;
+    }
+    ajax = new XMLHttpRequest();
     ajax.open(method_type,location,false);
     ajax.setRequestHeader('X-Requested-With : XMLHttpRequest');
+    ajax.setRequestHeader('X-Crsf : '+crsf_token);
     ajax.send(data);
-    var response=ajax.response;
+    var response=JSON.parse(ajax.response);
+
     return response;
 
+}
+function ceil(num){
+    return Math.ceil(num);
+}
+function substr(str,start,end){
+    return str.substring(start,end);
+}
+
+function bin_int(bits){
+    return parseInt(bits,2);
+}
+function int_bin(ints){
+    return ints.toString(2);
+}
+function int_to_bits(ints){
+    var bin=ints.toString(2);
+    var bits=bin.length;
+    return bits;
 }
