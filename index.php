8b031811b85b47222d0ff287b0faab0fbc7efac3
Macarthur Inbody <133794m3r@gmail.com>
2019-06-16 16:59:08 -0400

    need to put this in here so that when I use it, it's there.

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+</head>
+<?PHP
+session_start();
+$_SESSION['admin']=true;
+?>
+    <body>
+        <p>
+			This page is going to be used for the login page in the future but isn't ready for that yet. So for now it simply sets a cookie for me.
+        </p>
+        <p>
+
+        </p>
+    </body>
+</html>
